a557e383eeb003056512a18ea5199a4aef9e4c71
Tiffany8 <tiffany@TRWilliams.local>
2015-05-11 21:53:11 -0700

    barebones of the model.py file.

---
+++ model.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the SQLite database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+##############################################################################
+# Part 1: Compose ORM
+
+
+# End Part 1
+##############################################################################
+# Helper functions
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projectwun.db'
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
