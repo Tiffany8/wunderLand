3ccd1d828cd9a1027ac9608406975c873ac8b151
Tiffany8 <tiffany@TRWilliams.local>
2015-06-01 15:27:54 -0700

    code filters out stopwords from tokens

--- book_cosine_similarity.py
+++ book_cosine_similarity.py
@@ -1,16 +1,43 @@
+from server import app
+from model import Book, Author, Location, Category, Event, Award, connect_to_db, db
+from flask_sqlalchemy import SQLAlchemy
+
 import nltk
 import string
 import re
+from nltk.corpus import stopwords
 
 def main_func():
 	list_of_book_objects = Book.query.filter(Book.title.ilike('war%')).all()
-	for book_obj in list_of_book_objects:
-		tokens = get_tokens()
-		print tokens
 
-def get_tokens():
-			#remove all punctuation except ' and - and lowercases words
-			book_descrip_no_punc = re.split(r"[^\w'-]", book_obj.description.lower()) 
-			#or should I remove all punctuation, including apostrophes and dashes?
-			
-			return book_descrip_no_punc
+	for book_obj in list_of_book_objects:
+
+		if book_obj.description:
+
+			print "book title", book_obj.title
+			tokens_list = get_tokens(book_obj)
+			print "tokens list", len(tokens_list), tokens_list
+			print "#" * 20
+			tokens_list_filtered = remove_stopwords(tokens_list)
+			print "filtered tokens list", len(tokens_list_filtered), tokens_list_filtered
+			print "#" * 40
+
+def get_tokens(book_obj):
+
+		#remove all punctuation except ' and - and lowercases words
+		book_descrip_no_punc = re.split(r"[^\w]", book_obj.description.lower()) 
+		#or should I remove all punctuation, including apostrophes and dashes?
+		# description = book_obj.description.lower()
+		# book_descrip_no_punc = description.translate(None, string.punctuation)
+		return book_descrip_no_punc
+
+def remove_stopwords(tokens_list):
+
+	tokens_list_filtered = [word for word in tokens_list if not word in stopwords.words('english')]
+	
+	return tokens_list_filtered
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    db.create_all()
+    main_func()
