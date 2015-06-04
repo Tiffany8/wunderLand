c65be2aaed714984378d979c0856684fc260468b
Tiffany8 <tiffany@TRWilliams.local>
2015-06-04 15:23:21 -0700

    bootstrap modals are displaying. now working on getting them to show information unique to each thumbnail click.

--- static/projectwun.js
+++ static/projectwun.js
@@ -28,40 +28,82 @@ function getBookResults(evt) {
 
   var url = "/search?" + $('#user_book_query').serialize();
 
-  $.get(url, function(json) {
-    // console.log(json.name);
-    console.log(json.kmeans);
-    $("#results-div").append("<div class='container'><p>Found " + json.name.length + " books associated with " + $("#search-input").val() + ".</p></div>");
-    // $("#user-location-query").append($("#user_book_query").val());
+  $.get(url, function(json) { // both
+
+
+    $("#results-div").append("<div class='container'><p>Found " + json.name.length + " books associated with " + $("#search-input").val() + ".</p></div>"); //json result callback
+
     for (var book=0; book <json.name.length; book++) {
-    //     console.log(book);
-        $("#book-result-div").append("<div><img id='bookcover' class='book-thumbnail' src='" + json.name[book].thumbnailUrl + "'></div></div>");
-            
+        if (book%3===0) {
+          $("#book-result-div").append("<div class='row'><div class='col-md-4'><a data-toggle='modal' data-target='#myModal' href='#'><img id='" + json.name[book].isbn + "' class='book-thumbnail' src='" + json.name[book].thumbnailUrl + "'></a></div>");
+        } else if (book%3===2) {
+          $("#book-result-div").append("<div class='col-md-4'><a data-toggle='modal' data-target='#myModal' href='#'><img id='" + json.name[book].isbn + "' class='book-thumbnail' src='" + json.name[book].thumbnailUrl + "'></a></div></div>"); 
+        } else {
+          $("#book-result-div").append("<div class='col-md-4'><a data-toggle='modal' data-target='#myModal' href='#'><img id='" + json.name[book].isbn + "' class='book-thumbnail' src='" + json.name[book].thumbnailUrl + "'></a></div>"); 
+        }
+        
+            // data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo"
 
-      }
-      $("#kmeans_graph").append(json.kmeans);
-      // $(".jumbotron").click(function() {
-      // console.log("hello")
-      // // $('#imagepreview').attr('src', $('#bookcover').attr('src'));
-      // $('#imagemodal').show();
-   
-      // });
+      // $('#myModalLabel').append(json.name[book].title);
+      // $('#imagepreview').attr('src', $('#bookcover').attr('src'));
+      // $('#author').append("Author: " + json.name[book].authors);
+      // $('#description').append("Description: " + json.name[book].description);
 
+    }
+  });
+}
+// create a function that is tied to the creation of the thumbnail --- what's going to change is that book's personal info
+// clear the div at the beginning of the function 
+
+function getKMeansResults(evt) {
+  evt.preventDefault();
+  $('#results-div').empty();
+  $('#book-result-div').empty();    
+
+  var url = "/search/kmeans?" + $('#user_book_query').serialize();
+
+  $.get(url, function(json) { // both
+
+      
+      $("#kmeans_graph").append(json.kmeans); // kmeans result callback
+    
   });
 }
 
 
 $(document).ready(function () {
   $('#user_book_query').on('submit', getBookResults);
+  $('#user_book_query').on('submit', getKMeansResults)
   $('#books').tab('show')
+  // $("#book-thumbnail").on('click', function () { $('#myModal').modal("show")});
   $('#myTab a:first').click(function (e) {
     e.preventDefault()
     $(this).tab('show')
   });
+
   $('#myTab a:last').click(function (e) {
     e.preventDefault()
     $(this).tab('show')
   });
+
+  // The timer for the loading screen - starts 1000 ms after call
+  $(document).ajaxStart(function () {
+      timer = setTimeout(function () {
+        $("#loaderthingy").show();
+      }, 1000);
+  });
+
+  $(document).ajaxComplete(function () {
+      $("#loaderthingy").hide();
+      clearTimeout(timer);
+  });
+
+   
+  // $("#book-thumbnail").on('click', function() {
+  //   $('#myModal').modal('show');
+  // });
+  // $("#book-thumbnail").modal("show");
+
 })
 
 
@@ -88,6 +130,4 @@ $(document).ready(function () {
 
 
 // $("#book-result-div").on("click", getBookModal);
-
-
 
