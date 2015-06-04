4ae6ae8ecc347499de5fe49e366055da73e34f1f
Tiffany8 <tiffany@TRWilliams.local>
2015-06-03 19:39:04 -0700

    Updated the book cosine to generate the mpld3 graph and it also takes in the user input query from the server.py file.

--- book_cosine_similarity.py
+++ book_cosine_similarity.py
@@ -34,13 +34,13 @@ from pylab import *
 
 
 
-def main_func():
+def main_func(list_of_book_objects):
 	# current_time = datetime.datetime.utcnow()
 	# onehund_weeks_ago = current_time - datetime.timedelta(weeks=200)
 	# list_of_book_objects = Book.query.filter(Book.publication_date > onehund_weeks_ago).all()
 	# list_of_book_objects = Book.query.filter(Book.title.ilike('war%')).all()
 	list_of_book_objects = Location.get_books_associated_with_location(100, "Los Angeles")
-	# list_of_book_objects = [book_obj for book_obj in list_of_book_objects if book_obj.locations]
+	list_of_book_objects = [book_obj for book_obj in list_of_book_objects if book_obj.locations]
 	
 	print "NUMBER OF BOOKS, ", len(list_of_book_objects)
 	bookobj_tokens_dict = {}
@@ -62,8 +62,12 @@ def main_func():
 			bookobj_tokens_dict[book_obj] = stemmed_tokens
 
 	kmeans_variables = tfidf_similarity(bookobj_tokens_dict)
-	kmeans_cluster(*kmeans_variables)
-	
+	# km, dist, num_clusters = kmeans_cluster(*kmeans_variables)
+	kmeans_cluster_graph_html = kmeans_cluster(*kmeans_variables)
+
+	# graph_values, graph_keys = get_top_terms_per_cluster(km, bookobj_tokens_dict, num_clusters)
+	# kmeans_cluster_graph_html = multi_diminsional_scaling_for_2D_array(dist, graph_values, graph_keys)
+	return kmeans_cluster_graph_html
 
 def get_tokens(book_description):
 	"""Take book description, strip of punctuation, lowercase, and split/token into individual
@@ -142,7 +146,7 @@ def kmeans_cluster(terms, description_tokens, tfidf_matrix, titles, bookobj_toke
 	# #cosine distance which I will use for plotting on a euclidean (2-dimensional) plane.
 	dist = 1 - cosine_similarity(tfidf_matrix)
 	
-	num_clusters = 9 # if this number is changed, update the cluster_color dictionary
+	num_clusters = 8 # if this number is changed, update the cluster_color dictionary
 	# km = KMeans(init='k-means++', t
 	# 			max_iter=300, 
 	# 			n_init=20,
@@ -160,8 +164,11 @@ def kmeans_cluster(terms, description_tokens, tfidf_matrix, titles, bookobj_toke
 	frame = pd.DataFrame(books,index=[clusters],columns=['title','cluster'])
 	frame['cluster'].value_counts()
 	grouped = frame['title'].groupby(frame['cluster'])
-	
 
+	# return km, dist, num_clusters
+
+	
+# def get_top_terms_per_cluster(km, bookobj_tokens_dict, num_clusters):
 	#############################
 	### Top Terms Per Cluster ###
 	#############################
@@ -169,7 +176,7 @@ def kmeans_cluster(terms, description_tokens, tfidf_matrix, titles, bookobj_toke
 	print 
 	order_centroids = km.cluster_centers_.argsort()[:, ::-1]
 	print "order centroids, ", order_centroids
-
+	
 	##vocab frame --- don't understand yet
 	totalvocab_tokenized_list = [word for token_sublist in bookobj_tokens_dict.values() for word in token_sublist]
 	print "total vocab tokenized list, ", totalvocab_tokenized_list
@@ -186,7 +193,8 @@ def kmeans_cluster(terms, description_tokens, tfidf_matrix, titles, bookobj_toke
 	for i in range(num_clusters):
 		print "Cluster %d words:" % i
 		the_terms = []
-		for ind in order_centroids[i, :6]: # top 5 words that are nearest to the cluster centroid
+
+		for ind in order_centroids[i, :3]: # top 5 words that are nearest to the cluster centroid
 			
 			graph_terms = vocab_frame.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8','ignore')
 			the_terms.append(graph_terms)
@@ -208,7 +216,9 @@ def kmeans_cluster(terms, description_tokens, tfidf_matrix, titles, bookobj_toke
 		print 
 		print
 	print "graph values ", graph_values
+	# return graph_values, graph_keys
 
+	# def multi_diminsional_scaling_for_2D_array(dist, graph_values, graph_keys):
 	#################################
 	### Multi-Dimensional Scaling ###
 	#################################
@@ -231,48 +241,145 @@ def kmeans_cluster(terms, description_tokens, tfidf_matrix, titles, bookobj_toke
 
 	# #set up colors per clusters using a dict
 	cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e', 
-					5:'#F7EC45', 6:'#2ee3b6', 7:'#cd82c0', 8:'#a6efa4'}
-	# cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e'}
+					5:'#F7EC45', 6:'#2ee3b6', 7:'#cd82c0'}
+	# cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e', , 8:'#a6efa4'}
 
 	# #set up cluster names using a dict
 	graph_values = [", ".join(term_list) for term_list in graph_values]
 	cluster_names = dict(zip(graph_keys, graph_values))
 	print "cluster names dictionary, ", cluster_names
 
-	df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=titles))
-	
-	# #group by clusters
+
+	#create data frame that has the result of the MDS plus the cluster numbers and titles
+	df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=titles)) 
+
+	#group by cluster
 	groups = df.groupby('label')
 
-	# #set up plot
-	fig, ax = plt.subplots(figsize=(17,9)) #set size
-	ax.margins(0.05) # optional, just adds 5% padding to auto scaling
+	#define custom css to format the font and to remove the axis labeling
+	css = """
+	text.mpld3-text, div.mpld3-tooltip {
+	  font-family:Arial, Helvetica, sans-serif;
+	}
 
-	# #iterate through groups to layer the plot
-	# #note the use of cluster_name and cluster_color dicts with the 'name' lookup to return 
-	# #appropriate color/label
+	g.mpld3-xaxis, g.mpld3-yaxis {
+	display: none; }
+
+	svg.mpld3-figure {
+	margin-left: -200px;}
+	"""
+
+	# Plot 
+	fig, ax = plt.subplots(figsize=(14,6)) #set plot size
+	ax.margins(0.03) # Optional, just adds 5% padding to the autoscaling
+
+	#iterate through groups to layer the plot
+	#note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
 	for name, group in groups:
-		ax.plot(group.x, group.y, marker='o', linestyle='', ms=12,
-			label=cluster_names[name], color=cluster_colors[name],
-			mec='none')
-		ax.set_aspect('auto')
-		ax.tick_params(axis='x',
-			which='both',
-			bottom='off',
-			top='off',
-			labelleft='off')
+	    points = ax.plot(group.x, group.y, marker='o', linestyle='', ms=18, 
+	                     label=cluster_names[name], mec='none', 
+	                     color=cluster_colors[name])
+	    ax.set_aspect('auto')
+	    labels = [i for i in group.title]
+	    
+	    #set tooltip using points, labels and the already defined 'css'
+	    tooltip = mpld3.plugins.PointHTMLTooltip(points[0], labels,
+	                                       voffset=10, hoffset=10, css=css)
+	    #connect tooltip to fig
+	    mpld3.plugins.connect(fig, tooltip, TopToolbar())    
+	    
+	    #set tick marks as blank
+	    ax.axes.get_xaxis().set_ticks([])
+	    ax.axes.get_yaxis().set_ticks([])
+	    
+	    #set axis as blank
+	    ax.axes.get_xaxis().set_visible(False)
+	    ax.axes.get_yaxis().set_visible(False)
 
-	ax.legend(numpoints=1)
+	    
+	ax.legend(numpoints=1) #show legend with only one dot
 
-	for i in range(len(df)):
-		ax.text(df.ix[i]['x'], df.ix[i]['y'], df.ix[i]['title'],size=8)
+	mpld3.display() #show the plot
 
-	plt.show()
+	#uncomment the below to export to html
+	kmeans_cluster_graph_html = mpld3.fig_to_html(fig)
+	print(kmeans_cluster_graph_html)
+	return kmeans_cluster_graph_html
+
+
+	#######################################################
+	### Code for visualizing using matplotlib (minus d3)###
+	#######################################################
+	# df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=titles))
+	
+	# # #group by clusters
+	# groups = df.groupby('label')
+
+	# # #set up plot
+	# fig, ax = plt.subplots(figsize=(17,9)) #set size
+	# ax.margins(0.05) # optional, just adds 5% padding to auto scaling
+
+	# # #iterate through groups to layer the plot
+	# # #note the use of cluster_name and cluster_color dicts with the 'name' lookup to return 
+	# # #appropriate color/label
+	# for name, group in groups:
+	# 	ax.plot(group.x, group.y, marker='o', linestyle='', ms=12,
+	# 		label=cluster_names[name], color=cluster_colors[name],
+	# 		mec='none')
+	# 	ax.set_aspect('auto')
+	# 	ax.tick_params(axis='x',
+	# 		which='both',
+	# 		bottom='off',
+	# 		top='off',
+	# 		labelleft='off')
+
+	# ax.legend(numpoints=1)
+
+	# for i in range(len(df)):
+	# 	ax.text(df.ix[i]['x'], df.ix[i]['y'], df.ix[i]['title'],size=8)
+
+	# return plt.show()
+
+
+
+
+
+
+#define custom toolbar location
+class TopToolbar(mpld3.plugins.PluginBase):
+    """Plugin for moving toolbar to top of figure"""
+
+    JAVASCRIPT = """
+    mpld3.register_plugin("toptoolbar", TopToolbar);
+    TopToolbar.prototype = Object.create(mpld3.Plugin.prototype);
+    TopToolbar.prototype.constructor = TopToolbar;
+    function TopToolbar(fig, props){
+        mpld3.Plugin.call(this, fig, props);
+    };
+
+    TopToolbar.prototype.draw = function(){
+      // the toolbar svg doesn't exist
+      // yet, so first draw it
+      this.fig.toolbar.draw();
+
+      // then change the y position to be
+      // at the top of the figure
+      this.fig.toolbar.toolbar.attr("x", 150);
+      this.fig.toolbar.toolbar.attr("y", 400);
+
+      // then remove the draw function,
+      // so that it is not called again
+      this.fig.toolbar.draw = function() {}
+    }
+    """
+    def __init__(self):
+        self.dict_ = {"type": "toptoolbar"}
+
 
 
 
 if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
-    main_func()
+    main_func(list_of_book_objects)
     # cosine_similarity = cosine_similarity(bookobj_tokens_dict)
