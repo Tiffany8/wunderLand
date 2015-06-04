1b3b204c791c9f7958aef7e61fc37aa12c7cddfa
Tiffany8 <tiffany@TRWilliams.local>
2015-06-04 10:49:09 -0700

    Added the keywords to the book_object dictionary that will be jsonified.  Created routes for keyword page and for kmeans graph.

--- server.py
+++ server.py
@@ -2,16 +2,16 @@ from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, redirect, request as flaskrequest, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from model import Book, Author, Location, Category, Award, Event, Character,\
+from model import Book, Author, Location, Category, Award, Event, Keyword, Character,\
         connect_to_db, db
 import os
 import json
-
+from book_cosine_similarity import *
 #from Google Books
 import pprint
 import sys
 from apiclient.discovery import build
-from book_cosine_similarity import *
+
 
 app = Flask(__name__)
 
@@ -55,17 +55,17 @@ def search_for_books():
     kmeans_cluster_html = main_func(book_obj_list)
 
     for book_object in book_obj_list:
-        authorlist = []
-        if book_object.authors:
-            for author in book_object.authors:
-                authorlist.append(author.author_name)
         book_dict = {}
+        author_list = [author.author_name for author in book_object.authors if book_object.authors]
         book_dict["title"] = book_object.title
+        print book_dict["title"]
         book_dict["subtitle"] = book_object.subtitle
-        book_dict["authors"] = ", ".join(authorlist)
+        book_dict["authors"] = ", ".join(author_list)
         book_dict["description"] = book_object.description
         book_dict["thumbnailUrl"] = book_object.thumbnail_url
         book_dict["previewLink"] = book_object.preview_link
+        book_dict["keywords"] = [word.keyword for word in book_object.keywords]
+        print book_dict["keywords"]
         jsonify_search_result_list.append(book_dict)
         # print book_dict
     # print jsonify_search_result_list
@@ -76,8 +76,15 @@ def search_for_books():
     return jsonify(name = jsonify_search_result_list, kmeans = kmeans_cluster_html)
 
 
-# @app.route("search/kmeans")
+@app.route("/search/kmeans")
+def get_kmeans():
+    kmeans_cluster_html = flaskrequest.args.get('kmeans_cluster_html')
+    return kmeans_cluster_html
 
+    
+@app.route("/keyword/<keyword>")
+def books_associated_with_keyword_page():
+    pass
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
