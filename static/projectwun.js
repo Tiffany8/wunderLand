65007eda803651d35b623d2dceefbbabc878ef3a
Tiffany8 <tiffany@TRWilliams.local>
2015-06-07 13:46:38 -0700

    added style to the google map

--- static/projectwun.js
+++ static/projectwun.js
@@ -188,8 +188,10 @@ function errorFunction(position) {
 
   
 
-
-
+var styles = [{"stylers":[{"saturation":-100}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#0099dd"}]},
+{"elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#aadd55"}]},
+{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels.text","stylers":[{"visibility":"on"}]},
+{"featureType":"road.local","elementType":"labels.text","stylers":[{"visibility":"on"}]},{}];
 
 
 
@@ -214,6 +216,7 @@ function initialize(coords) {
   });
 
   var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
+  map.setOptions({styles: styles});
   marker.setMap(map);
   
  
