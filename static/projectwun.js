93eae7082616ba1f41fa7ae165da8e50c15de7e8
Tiffany8 <tiffany@TRWilliams.local>
2015-06-06 19:03:50 -0700

    Created a working function that sends keyword button values to the server and for a query and returns back list of books associated with keyword.

--- static/projectwun.js
+++ static/projectwun.js
@@ -1,6 +1,5 @@
 // PART 1: SHOW Book results
 
-
 function getBookResults(evt) {
   evt.preventDefault();
   $('#results-div').empty();
@@ -15,44 +14,120 @@ function getBookResults(evt) {
     $("#results-div").append("<div class='container'><p>Found " + json.name.length + " books associated with " + $("#search-input").val() + ".</p></div>"); 
     $('.results-tabs').addClass('show');
     for (var book=0; book <json.name.length; book++) {
-      if (book%3===0) {
-        $("#book-result-div").append('<div class="row"><div class="col-md-4"><a data-toggle="modal" data-target="#myModal" href="#"><img id="' +  json.name[book].isbn + '" class="book-thumbnail" data-title="' + json.name[book].title + '" data-keywords="' + json.name[book].keywords + '" data-subtitle="' + json.name[book].subtitle + '" data-thumbnail="' + json.name[book].thumbnailUrl + '" data-description="' + json.name[book].description +'" data-author="' + json.name[book].authors + '" src="' + json.name[book].thumbnailUrl + '"></a></div>');
-        } else if (book%3===2) {
-        $("#book-result-div").append('<div class="col-md-4"><a data-toggle="modal" data-target="#myModal" href="#"><img id="' +  json.name[book].isbn + '" class="book-thumbnail" data-title="' + json.name[book].title + '" data-keywords="' + json.name[book].keywords + '" data-subtitle="' + json.name[book].subtitle + '" data-thumbnail="' + json.name[book].thumbnailUrl + '" data-description="' + json.name[book].description +'" data-author="' + json.name[book].authors + '" src="' + json.name[book].thumbnailUrl + '"></a></div></div>');
-        } else {
-        $("#book-result-div").append('<div class="col-md-4"><a data-toggle="modal" data-target="#myModal" href="#"><img id="' +  json.name[book].isbn + '" class="book-thumbnail" data-title="' + json.name[book].title + '" data-keywords="' + json.name[book].keywords + '" data-subtitle="' + json.name[book].subtitle + '" data-thumbnail="' + json.name[book].thumbnailUrl + '" data-description="' + json.name[book].description +'" data-author="' + json.name[book].authors + '" src="' + json.name[book].thumbnailUrl + '"></a></div>');
-        }
-    
-
-      $(".book-thumbnail").click(function() {
-        console.log("helelloeoeoeoooo")
-        $('.book-title').empty();
-        $('.book-keywords').empty();
-        $('.book-subtitle').empty();
-        $('.book-author').empty();
-        $('.book-thumbnailUrl').empty();
-        $('.book-description').empty();
-
-        var keywords = ($(this).data("keywords")).split(",");
-        for (var word=0; word<keywords.length; word++) {
-          $(".book-keywords").append("<a class='btn btn-success btn-xs' href='/keyword/" + keywords[word] + "'>" + keywords[word] + "</a>" + " ")
-          console.log(keywords[word]);
-        }
-        $(".book-title").append($(this).data("title"));
-        $('.book-subtitle').append($(this).data('subtitle'));
-        $('.book-author').append($(this).data('author'));
-        $('.book-description').append($(this).data('description'));
-        $('.book-thumbnailUrl').append("<img class='modal-cover' src='" + ($(this).data('thumbnail')) + "'>");
-
-      });
+     
+      $("#book-result-div").append('<div class="col-md-3"><a data-toggle="modal" data-target="#myModal" href="#"><img id="' +
+        json.name[book].isbn + '" class="book-thumbnail" data-title="' +
+         json.name[book].title + '" data-keywords="' + 
+         json.name[book].keywords + '" data-subtitle="' + 
+         json.name[book].subtitle + '" data-thumbnail="' + 
+         json.name[book].thumbnailUrl + '" data-description="' + 
+         json.name[book].description +'" data-author="' + 
+         json.name[book].authors + '" src="' + 
+         json.name[book].thumbnailUrl + '"></a></div>'
+      );   
     }
+
+    $(".book-thumbnail").click(function() {
+      
+      $('.book-title').empty();
+      $('.book-keywords').empty();
+      $('.book-subtitle').empty();
+      $('.book-author').empty();
+      $('.book-thumbnailUrl').empty();
+      $('.book-description').empty();
+
+      var keywords = ($(this).data("keywords")).split(",");
+      for (var word=0; word<keywords.length; word++) {
+        // var keywordUrl = "/keyword/" + $(keyword[word]).serialize.();
+        $(".book-keywords").append('<button type="button" class="btn btn-success btn-xs tester keywordbutton" data-key="' +
+          keywords[word] + '">' + keywords[word] + "</button>" + " ")
+   
+      }
+      $(".book-title").append($(this).data("title"));
+      $('.book-subtitle').append($(this).data('subtitle'));
+      $('.book-author').append($(this).data('author'));
+      $('.book-description').append($(this).data('description'));
+      $('.book-thumbnailUrl').append("<img class='modal-cover' src='" + ($(this).data('thumbnail')) + "'>");
+
+    }); 
+    
   });
 }
 
+// The end of getBookResults //
 
 
-//  func
 
+// Part 2: Get Keyword Associated Books
+ 
+
+$(document).on('click', '.keywordbutton', function() {
+  $('#results-div').empty();
+  $('#book-result-div').empty();
+
+  console.log("helelloeoeoeoooo")
+  var keyword = $(this).data('key');
+  var url = "/keyword?keyword=" + keyword
+  
+  console.log(keyword)
+  console.log(url)
+
+  $.get(url, function(json) {
+
+    // evt.preventDefault();
+    // $('#results-div').empty();
+    // $('#book-result-div').empty();
+
+    $("#results-div").append("<div class='container'><p>Found " + json.keywordbooks.length + 
+    " books associated with " + keyword + ".</p></div>"); 
+
+    $('.results-tabs').addClass('show');
+
+    for (var book=0; book <json.keywordbooks.length; book++) {
+     
+      $("#book-result-div").append('<div class="col-md-3"><a data-toggle="modal" data-target="#myModal" href="#"><img id="' +
+        json.keywordbooks[book].isbn + '" class="book-thumbnail" data-title="' +
+         json.keywordbooks[book].title + '" data-keywords="' + 
+         json.keywordbooks[book].keywords + '" data-subtitle="' + 
+         json.keywordbooks[book].subtitle + '" data-thumbnail="' + 
+         json.keywordbooks[book].thumbnailUrl + '" data-description="' + 
+         json.keywordbooks[book].description +'" data-author="' + 
+         json.keywordbooks[book].authors + '" src="' + 
+         json.keywordbooks[book].thumbnailUrl + '"></a></div>'
+      );   // append closer
+
+      $(".book-thumbnail").click(function() {
+      
+      $('.book-title').empty();
+      $('.book-keywords').empty();
+      $('.book-subtitle').empty();
+      $('.book-author').empty();
+      $('.book-thumbnailUrl').empty();
+      $('.book-description').empty();
+
+      var keywords = ($(this).data("keywords")).split(",");
+      for (var word=0; word<keywords.length; word++) {
+        // var keywordUrl = "/keyword/" + $(keyword[word]).serialize.();
+        $(".book-keywords").append('<button type="button" class="btn btn-success btn-xs tester keywordbutton" data-key="' +
+          keywords[word] + '">' + keywords[word] + "</button>" + " ")
+   
+      }
+      $(".book-title").append($(this).data("title"));
+      $('.book-subtitle').append($(this).data('subtitle'));
+      $('.book-author').append($(this).data('author'));
+      $('.book-description').append($(this).data('description'));
+      $('.book-thumbnailUrl').append("<img class='modal-cover' src='" + ($(this).data('thumbnail')) + "'>");
+
+      }); // modals book-thumbnail 
+    } // for loop closing
+  }); // get request closer
+});  // bookthumbnail click closer
+// }    // outermost bracket
+
+
+
+
+// Par 3: Geolocation
 
 function showPosition(){
         if(navigator.geolocation){
@@ -119,6 +194,23 @@ $(document).ready(function () {
       $("#loaderthingy").hide();
       clearTimeout(timer);
   });
+
+  // $('.keyword').on('click', getKeywordBooks);
+  // $(document).on('click', '.keywordbutton', function(event) {
+  //   alert("hello");
+
+  // });
+
+  // $('.keywordbutton').on('click', function() {
+  //   console.log('anybody here')
+  // })
+  $('.tester').on('click', function() {
+  console.log("what'sup");
+});
+  // $($(this).data('key')).on('click', function() {
+  //   console.log('hi')
+  // });
+
 })
 
 
