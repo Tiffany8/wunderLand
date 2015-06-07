9b26971e45462bf965210006e0f87bbb8fc0330e
Tiffany8 <tiffany@TRWilliams.local>
2015-06-06 19:00:13 -0700

    Switched keyword route -- get request to send keyword button action value to server.

--- server.py
+++ server.py
@@ -48,20 +48,24 @@ def search_for_books():
 
     ##hard code with LA cordinates due to quota limit:##
     book_obj_list = Location.get_books_associated_with_location(radius, user_location_query) #both
+    book_obj_list = [book_obj for book_obj in book_obj_list if book_obj.description]
+    book_obj_list = [book_obj for book_obj in book_obj_list if len(book_obj.description.split(" ")) > 20]
     print book_obj_list
 
 
     for book_object in book_obj_list:               # json result
         book_dict = {}
+        keyword_list = [word.keyword for word in book_object.keywords]
+        keywords = filter(None, keyword_list)
         author_list = [author.author_name for author in book_object.authors if book_object.authors]
-        book_dict["title"] = book_object.title
+        book_dict["title"] = book_object.title.replace('"', "'")
         print book_dict["title"]
         book_dict["subtitle"] = book_object.subtitle
         book_dict["authors"] = ", ".join(author_list)
-        book_dict["description"] = book_object.description
+        book_dict["description"] = book_object.description.replace('"', "'")
         book_dict["thumbnailUrl"] = book_object.thumbnail_url
         book_dict["previewLink"] = book_object.preview_link
-        book_dict["keywords"] = [word.keyword for word in book_object.keywords]
+        book_dict["keywords"] = keywords
         print book_dict["keywords"]
         jsonify_search_result_list.append(book_dict)
         # print book_dict
@@ -72,6 +76,15 @@ def search_for_books():
 
     return jsonify(name = jsonify_search_result_list)
 
+# @app.route("/results")
+# def search_results_display():
+
+#     return render_template("searchresults.html")
+
+# @app.route("/search/kmeans", methods=["POST"])
+# def kmeans_results_display():
+
+#     return render_template("searchresults.html")
 
 @app.route("/search/kmeans")
 def get_kmeans():
@@ -80,14 +93,45 @@ def get_kmeans():
     radius = flaskrequest.args.get('radius')
     print "radius for get kmeans", radius
     book_obj_list = Location.get_books_associated_with_location(radius, user_location_query)
+    book_obj_list = [book_obj for book_obj in book_obj_list if book_obj.description]
+    book_obj_list = [book_obj for book_obj in book_obj_list if len(book_obj.description.split(" ")) > 20]
     # from the book_cosine_similarity file
     kmeans_cluster_html = main_func(book_obj_list) # kmeans result
     return jsonify(kmeans = kmeans_cluster_html)
 
 
-@app.route("/keyword/<keyword>")
+@app.route("/keyword", methods=["GET"])
 def books_associated_with_keyword_page():
-    pass
+
+    keyword = flaskrequest.args.get('keyword')
+
+    jsonify_search_result_list = []
+    keyword_object_list = Keyword.query.filter_by(keyword=keyword).all()
+    book_obj_list_of_list = [key_obj.books for key_obj in keyword_object_list]
+    book_obj_list = [book_obj for sublist in book_obj_list_of_list for book_obj in sublist]
+    book_obj_list = [book_obj for book_obj in book_obj_list if book_obj.description]
+    book_obj_list = [book_obj for book_obj in book_obj_list if len(book_obj.description.split(" ")) > 20]
+    print book_obj_list
+
+    for book_object in book_obj_list:               # json result
+        book_dict = {}
+        keyword_list = [word.keyword for word in book_object.keywords]
+        keywords = filter(None, keyword_list)
+        author_list = [author.author_name for author in book_object.authors if book_object.authors]
+        book_dict["title"] = book_object.title
+        print book_dict["title"]
+        book_dict["subtitle"] = book_object.subtitle
+        book_dict["authors"] = ", ".join(author_list)
+        book_dict["description"] = book_object.description
+        book_dict["thumbnailUrl"] = book_object.thumbnail_url
+        book_dict["previewLink"] = book_object.preview_link
+        book_dict["keywords"] = keywords
+        print book_dict["keywords"]
+        jsonify_search_result_list.append(book_dict)
+
+    print "search complete"
+    
+    return jsonify(keywordbooks = jsonify_search_result_list, keyword=keyword)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
