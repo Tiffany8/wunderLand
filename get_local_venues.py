40db1876343ef8e493ee96ac707573c599441ffd
Tiffany8 <tiffany@TRWilliams.local>
2015-06-07 15:48:45 -0700

    Created a working parse_local_venues function that pares the local_venue XML and saves venue info in a dictionary.

--- get_local_venues.py
+++ get_local_venues.py
@@ -12,6 +12,11 @@ import xml.etree.ElementTree as ET
 
 apikey = os.environ['LIBRARYTHING_DEVELOP_KEY']
 
+def return_local_venues(latitude, longitude):
+	local_venues = get_local_venues(latitude, longitude)
+	ten_local_venues = parse_local_venues(local_venues)
+	return ten_local_venues
+
 def get_local_venues(latitude, longitude):
 
 	
@@ -19,16 +24,32 @@ def get_local_venues(latitude, longitude):
 	
 	print "longitude, ", longitude
 	arguments = {"method" : "librarything.local.getvenuesnear", "lat" : latitude, 
-	"lon": longitude, "distance":5, "venueType":1, "apikey" : apikey}
+	"lon": longitude, "distance":3, "venueType":1, "apikey" : apikey}
 	local_venues = requests.get('http://librarything.com/services/rest/1.1/', params=arguments).text
 	pp = pprint.PrettyPrinter(indent=4)
 	pp.pprint(local_venues)
 	return local_venues    
 
 def parse_local_venues(local_venues):
-	# root = ET.fromstring(local_venues)
- #    ns={'lt':'http://www.librarything.com/'}
-    pass
+	local_venues_dict = {}
+	root = ET.fromstring(local_venues)
+	ns={'lt':'http://www.librarything.com/'}
+	places = root.findall('./lt:ltml/lt:itemList/lt:item', ns)
+	for item in places:
+		name = item.find('lt:name', ns).text
+		website = item.find('lt:officialSite',ns).text
+		latitude = item.find('lt:location/lt:lat',ns).text
+		longitude = item.find('lt:location/lt:lng',ns).text
+		local_venues_dict[name] = {name=name,
+		}
+
+
+		local_venues_dict[name]
+
+		print "place, ", place
+
+
+
 
 if __name__ == "__main__":
     connect_to_db(app)
