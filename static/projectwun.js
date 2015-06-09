faabc7a247cdeccf8936c705d65daecf085034a3
Tiffany8 <tiffany@TRWilliams.local>
2015-06-09 12:48:21 -0700

    Moved search bar on search return from the search div to the navbar right div.

--- static/projectwun.js
+++ static/projectwun.js
@@ -5,12 +5,15 @@ function getBookResults(evt) {
   $('#results-div').empty();
   $('#book-result-div').empty();
 
+    // With JQuery
+  
 
-  var url = "/search?" + $('#user_book_query').serialize();
-
+  var url = "/search?" + $('.user_book_query').serialize(); 
   $.get(url, function(json) { 
 
-
+    var searchForm = $('#search-input-form').html();
+    $('#search-input-form').remove();
+    $('#navbar-search').append(searchForm);
     $("#results-div").append("<div class='container'><p>Found " + json.name.length + " books associated with " + $("#search-input").val() + ".</p></div>"); 
     $('.results-tabs').addClass('show');
     for (var book=0; book <json.name.length; book++) {
@@ -133,7 +136,7 @@ function getModalInfoForEachBook(evt) {
   $('.book-title').empty();
   $('.book-keywords').empty();
 
-  var url='/search?' + $('#user_book_query').serialize();
+  var url='/search?' + $('.user_book_query').serialize();
 
   $.get(url, function(json) {
     console.log(json)
@@ -147,7 +150,7 @@ function getKMeansResults(evt) {
   $('#book-result-div').empty();
   $('#kmeans_graph').empty();    
 
-  var url = "/search/kmeans?" + $('#user_book_query').serialize();
+  var url = "/search/kmeans?" + $('.user_book_query').serialize();
 
   $.get(url, function(json) { 
     $("#kmeans_graph").append(json.kmeans); 
@@ -218,20 +221,9 @@ function initialize(coords, bookStores) {
   // Marker sizes are expressed as a Size of X,Y
   // where the origin of the image (0,0) is located
   // in the top left of the image.
-
   // Origins, anchor positions and coordinates of the marker
   // increase in the X direction to the right and in
   // the Y direction down.
-  // var image = {
-  //   url:'http://www.icon2s.com/wp-content/uploads/2013/07/black-white-metro-books-icon.png',
-  //   // This marker is 20 pixels wide by 32 pixels tall.
-  //   size: new google.maps.Size(20, 32),
-  //   // The origin for this image is 0,0.
-  //   origin: new google.maps.Point(0,0),
-  //   // The anchor for this image is the base of the flagpole at 0,32.
-  //   anchor: new google.maps.Point(0, 32)
-  // };
-
   var bookIcon = {
     path: "M 461.754,51.435c0-3.902-1.822-7.581-4.927-9.945c-3.105-2.364-7.132-3.144-10.896-2.105L230.877,98.694L15.823,39.384 c-3.762-1.037-7.792-0.258-10.896,2.105C1.823,43.853,0,47.531,0,51.434v298.615c0,5.624,3.755,10.555,9.177,12.05l216.934,59.829 c1.468,0.583,3.031,0.892,4.611,0.892c0.052,0,0.104-0.005,0.155-0.006c0.052,0.001,0.104,0.006,0.154,0.006 c1.58,0,3.143-0.309,4.609-0.891L452.577,362.1c5.422-1.495,9.177-6.427,9.177-12.05V51.435z M25,67.849l193.221,53.289v272.683 L25,340.53V67.849z M436.754,340.53l-193.221,53.29V121.138l193.221-53.289V340.53 z",
     fillColor: '#000000',
@@ -239,8 +231,11 @@ function initialize(coords, bookStores) {
     scale: 0.05,
     strokeColor: 'black',
     strokeWeight: 2,
+    // This marker is 1 pixels wide by 4 pixels tall.
     size: new google.maps.Size(1,4),
+    // The origin for this image is 0,0.
     origin: new google.maps.Point(0,0),
+    // The anchor for this image is the base of the book at 0,32.
     anchor: new google.maps.Point(0,32)
   };
   // Shapes define the clickable region of the icon.
@@ -265,17 +260,34 @@ function initialize(coords, bookStores) {
         icon: bookIcon,
         shape: shape,
         title: bookstore[0],
+        website: bookstore[4],
         zIndex: bookstore[3]
-    });
-  }
-
-  google.maps.event.addListener(marker, 'click', function() {
-      
-    infowindow.setContent(contentString);
-    infowindow.open(map, marker);
     
-  }); 
-};
+
+
+    }); //end of marker creation
+  } //end of bookSTore for loop
+
+  // Creating an InfoWindow object
+  infowindow = new google.maps.InfoWindow({
+    content: "holding..."
+  });
+
+  for (var i = 0; i < markers.length; i++) {
+
+    var marker = markers[i];
+      google.maps.event.addListener(marker, 'click', function () {
+
+      // where I have added .html to the marker object.
+      infowindow.setContent(this.html);
+      infowindow.open(map, this);
+    }); //end of addlistener for marker
+    
+  } //end of for loop for markers.length
+
+
+  
+}; //end of the initialize function
 
 // function setMarkers(map, locations) {
 //   // Add markers to the map
@@ -327,11 +339,11 @@ function initialize(coords, bookStores) {
 
 // DOCUMENT READY 
 $(document).ready(function () {
-  $('#user_book_query').on('submit', getBookResults);
-  $('#user_book_query').on('submit', getKMeansResults);
+  $('.user_book_query').on('submit', getBookResults);
+  $('.user_book_query').on('submit', getKMeansResults);
   
 
-  $('#user_book_query').on('submit', function() {
+  $('.user_book_query').on('submit', function() {
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successFunction);
@@ -359,6 +371,9 @@ $(document).ready(function () {
     $(this).tab('show')
   });
 
+
+
+
   // The timer for the loading screen - starts 1000 ms after call
   $(document).ajaxStart(function () {
       timer = setTimeout(function () {
@@ -367,6 +382,17 @@ $(document).ready(function () {
   });
 
   $(document).ajaxComplete(function () {
+      $("#loaderthingy").hide();
+      clearTimeout(timer);
+  });
+
+  $('#local_venues').ajaxStart(function() {
+    timer = setTimeout(function() {
+      $('#loaderthingy').show();
+    }, 1000);
+  });
+
+  $('#local_venues').ajaxComplete(function () {
       $("#loaderthingy").hide();
       clearTimeout(timer);
   });
@@ -384,7 +410,8 @@ $(document).ready(function () {
   console.log("what'sup");
   });
 
-
+  $('#books').scroll();
+  $("#books").animate({ scrollTop: 1000 }, 2000);
 
   
 
